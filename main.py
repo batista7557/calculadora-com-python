5b28a80f0a66ed243bb411ae8953f560164e84a1
batista7557 <gabriel.bat2001@gmail.com>
2023-03-07 23:35:32 -0300

    Escrevendo no visor e função de limpar a tela

--- main.py
+++ main.py
@@ -13,38 +13,38 @@ class Calc:
         self.frame.pack()
 
         self.button_1 = Button(self.frame, bg="orange", bd=0, text="1", font="arial 20 bold", fg="white",
-                               width=5, height=3, command=None)
+                               width=5, height=3, command=lambda: self.touch("1"))
         self.button_2 = Button(self.frame, bg="orange", bd=0, text="2", font="arial 20 bold", fg="white",
-                               width=5, height=3, command=None)
+                               width=5, height=3, command=lambda: self.touch("2"))
         self.button_3 = Button(self.frame, bg="orange", bd=0, text="3", font="arial 20 bold", fg="white",
-                               width=5, height=3, command=None)
+                               width=5, height=3, command=lambda: self.touch("3"))
         self.button_4 = Button(self.frame, bg="orange", bd=0, text="4", font="arial 20 bold", fg="white",
-                               width=5, height=3, command=None)
+                               width=5, height=3, command=lambda: self.touch("4"))
         self.button_5 = Button(self.frame, bg="orange", bd=0, text="5", font="arial 20 bold", fg="white",
-                               width=5, height=3, command=None)
+                               width=5, height=3, command=lambda: self.touch("5"))
         self.button_6 = Button(self.frame, bg="orange", bd=0, text="6", font="arial 20 bold", fg="white",
-                               width=5, height=3, command=None)
+                               width=5, height=3, command=lambda: self.touch("6"))
         self.button_7 = Button(self.frame, bg="orange", bd=0, text="7", font="arial 20 bold", fg="white",
-                               width=5, height=3, command=None)
+                               width=5, height=3, command=lambda: self.touch("7"))
         self.button_8 = Button(self.frame, bg="orange", bd=0, text="8", font="arial 20 bold", fg="white",
-                               width=5, height=3, command=None)
+                               width=5, height=3, command=lambda: self.touch("8"))
         self.button_9 = Button(self.frame, bg="orange", bd=0, text="9", font="arial 20 bold", fg="white",
-                               width=5, height=3, command=None)
+                               width=5, height=3, command=lambda: self.touch("9"))
 
         self.button_increase = Button(self.frame, bg="orange", bd=0, text="+", font="arial 20 bold", fg="white",
-                               width=5, height=3, command=None)
+                               width=5, height=3, command=lambda: self.touch("+"))
         self.button_unincrease = Button(self.frame, bg="orange", bd=0, text="-", font="arial 20 bold", fg="white",
-                               width=5, height=3, command=None)
+                               width=5, height=3, command=lambda: self.touch("-"))
         self.button_division = Button(self.frame, bg="orange", bd=0, text="/", font="arial 20 bold", fg="white",
-                               width=5, height=3, command=None)
+                               width=5, height=3, command=lambda: self.touch("/"))
         self.button_multi = Button(self.frame, bg="orange", bd=0, text="*", font="arial 20 bold", fg="white",
-                               width=5, height=3, command=None)
+                               width=5, height=3, command=lambda: self.touch("*"))
 
         self.button_equal = Button(self.frame, bg="orange", bd=0, text="=", font="arial 20 bold", fg="white",
-                               width=10, height=3, command=None)
+                               width=10, height=3, command= lambda: self.touch("="))
 
         self.button_clear = Button(self.frame, bg="orange", bd=0, text="C", font="arial 20 bold", fg="white",
-                               width=5, height=3, command=None)
+                               width=5, height=3, command=self.Clean)
 
         self.button_1.grid(row=0, column=0)
         self.button_2.grid(row=0, column=1)
@@ -63,5 +63,11 @@ class Calc:
         self.button_equal.grid(row=3, column=0, columnspan=2)
 
         self.window.mainloop()
+
+    def touch(self, num):
+        self.screen_numbers.insert(END, num)
+
+    def Clean(self):
+        self.screen_numbers.delete(0, END)
 
 Calc()
